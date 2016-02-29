cf461abf6a57cde5a7b6689eb763531d0e568fd6
Tegan Wilson <teganwilson@eduroam-170-135.dyn.carleton.edu>
2016-02-28 19:07:24 -0600

    Tory and Toast classes with partially implemented methods

--- src/toastGame/Toast.java
+++ src/toastGame/Toast.java
@@ -1,29 +1,52 @@
 package toastGame;
 
+import java.util.*
+
 public class Toast {
+    private ArrayList<String>;
+    private int toastiness;
 
     public Toast() {
         //creates the toast object
+        //Toast starts out plain (0 toastiness and no toppings)
+        
+        this.toppings = new ArrayList<String>();
+        this.toastiness = 0;
     }
     
     public void addTopping(String topping) {
         //adds the topping listed to the toast
+        //might want to check (with assert?) that the topping being added is a legitimate topping option
+        
+        this.toppings.add(topping);
     }
     
-    public String[] getToppings() {
+    public ArrayList<String> getToppings() {
         //returns the current toppings on the toast in a list of strings
         //if topping become their own class, this would be adjusted to resturn a list of topping objects. But I don't know if that will be necessary...
+        
+        return this.toppings;
     }
     
     public void toast(int toastiness) {
         //toasts the Toast based on toastiness
         //should check somewhere (perhaps this will be done somewhere else) to ensure that the int is >= 0, and that it isn't less than the current toastiness
+        
+        this.toastiness = toastiness;
     }
     
     public int get toastiness() {
         //returns toastiness
+        
+        return this.toastiness;
     }
     
-    
+    public void clear() {
+        //resets toastiness to 0 and toppings list to empty
+        //The user should not be able to clear their toast when making it
+        
+        this.toastiness = 0;
+        this.toppings.clear();
+    }
     
 }
--- src/toastGame/Tory.java
+++ src/toastGame/Tory.java
@@ -1,23 +1,35 @@
 package toastGame;
 
 public class Tory {
+    private Toast criteria;
 
     public Tory() {
         //creates the Tory object
         //sets Tory's toast criteria to be plain toast
+        
+        this.criteria = new Toast();
     }
     
     public Toast createCriteria() {
         //creates a randomized Toast object to judge upon and returns it
         //should create toast objects based on difficulty of the game (implement this later)
+        
+        this.criteria.clear();
+        //This is where we would randomly get rid of toppings and set the toastiness of the criteria
+        return this.criteria
     }
     
     public Toast getCriteria() {
         //returns a toast object of Tory's current criteria
+        
+        return this.criteria;
     }
  
     public String judgeToast(Toast userToast) {
-        //takes in the user's toast and judges it based on Tory's current criteria
+        //takes in the user's toast and judges it based on Tory's current criteria (implement later)
+        //Could judge differently based on difficulty of level
+        
+        return '';
     }
     
 }
