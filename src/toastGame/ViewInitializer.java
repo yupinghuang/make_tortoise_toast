a18119e18b67c225a965dee587283b0ec439df28
Yuping Huang <yupinghyper@gmail.com>
2016-03-01 01:20:11 -0600

    Add topping image to toast working partially; need to implement random positioning

--- src/toastGame/ViewInitializer.java
+++ src/toastGame/ViewInitializer.java
@@ -1,23 +1,16 @@
-/**
- * ViewInitializer.java
- * Authors: Yuping Huang, Tegan Wilson, and Carolyn Ryan
- * Class structure taken from https://blogs.oracle.com/jmxetc/entry/connecting_scenebuilder_edited_fxml_to
- * Date: Feb 28, 2016
- * 
- * ViewInitializer class helps set up the GUI (View)
- */
-
+/** * ViewInitializer.java * Authors: Yuping Huang, Tegan Wilson, and Carolyn Ryan * Class structure taken from https://blogs.oracle.com/jmxetc/entry/connecting_scenebuilder_edited_fxml_to * Date: Feb 28, 2016 * * ViewInitializer class helps set up the GUI (View) */
 package toastGame;
 
 import java.net.URL;
-import java.util.ArrayList;
+import java.util.Random;
 import java.util.ResourceBundle;
-
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.shape.Rectangle;
@@ -29,123 +22,131 @@ public class ViewInitializer implements Initializable {
 	private static final int BUTTONSIZE_Y = 45;
 	private static final int BUTTONSIZE_X = 134;
 	private static final String SUBMIT = "javafx.event.ActionEvent[source=Button[id=submitButton, styleClass=button]'SUBMIT']";
-	
-	//FXML Variable Injections
-	@FXML private Pane buttonPane;
-	@FXML private Button submitButton;
-	@FXML private StackPane toastStackPane;
-	@FXML private StackPane sideButtonPane;
-	
+
+	// FXML Variable Injections
+	@FXML
+	private Pane buttonPane;
+	@FXML
+	private Button submitButton;
+	@FXML
+	private StackPane toastStackPane;
+	@FXML
+	private StackPane sideButtonPane;
+
 	// Instance Variables
-	private ArrayList<Topping> toppings;
 	private GameController myController;
-	private ArrayList<Button> buttonList;
-	
-	//Used later as an example of how to add to a stackpane
+	private Button[] buttonList;
+
+	// Used later as an example of how to add to a stackpane
 	private Rectangle exampleRectangle;
+
 	/**
-	 * Defines instance variables and creates topping buttons. 
+	 * Defines instance variables and creates topping buttons.
 	 */
 	protected void setUp() {
-		toppings = new ArrayList<Topping>();
-		buttonList = new ArrayList<Button>();
-		
+
 		myController = new GameController();
-		myController.getToppings();
-		
-		/* As we have not yet implemented the full extent of our model,
-		 * we will currently hard code the example for our GUI.
-		 * Creating the buttons and adding them to the buttonList will eventually be 
-		 * randomized by our GameController.  (TOBEDELETED)
-		 * 
-		 * Currently our interface is limited to six topping buttons, but this can change.
-		 */
-		Button one = new Button("Butter");
-		Button two = new Button("Jam");
-		Button three = new Button("Cinnamon&Sugar");
-		Button four = new Button("Peanut Butter");
-		Button five = new Button("Honey");
-		Button six = new Button("Avocado");
-		buttonList.add(one);
-		buttonList.add(two);
-		buttonList.add(three);
-		buttonList.add(four);
-		buttonList.add(five);
-		buttonList.add(six);
-		
-		// For the stack pane example.  (TOBEDELETED)
+		myController.registerView(this);
+		buttonList = myController.createButtons(6);
+
+		// For the stack pane example. (TOBEDELETED)
 		exampleRectangle = new Rectangle();
-        exampleRectangle.setX(50);
-        exampleRectangle.setY(50);
-        exampleRectangle.setWidth(200);
-        exampleRectangle.setHeight(100);
+		exampleRectangle.setX(50);
+		exampleRectangle.setY(50);
+		exampleRectangle.setWidth(200);
+		exampleRectangle.setHeight(100);
 	}
-	
+
 	/**
-	 * Arranges Buttons in a stack pane.
-	 * This stack pane is placed on the 
+	 * Arranges Buttons in a stack pane. This stack pane is placed on the
 	 */
 	@FXML
 	protected void setButtons() {
-		//Add buttons in list to the pane
+		// Add buttons in list to the pane
 		buttonPane = new Pane();
-		for (int i=0; i< buttonList.size(); i++) {
-			buttonPane.getChildren().add(buttonList.get(i));
+		for (int i = 0; i < buttonList.length; i++) {
+			buttonPane.getChildren().add(buttonList[i]);
 		}
-		//Add the button pane to the side pane
+		// Add the button pane to the side pane
 		sideButtonPane.getChildren().add(buttonPane);
 	}
-	
+
 	/**
-	 * Handles button action for submit button.
-	 * The submit button is implemented in the .fxml file.
-	 * As such, it can't be handled in the same method as 
-	 * the other buttons.
-	 * @param event		The event that needs to be handled
+	 * Handles button action for submit button. The submit button is implemented
+	 * in the .fxml file. As such, it can't be handled in the same method as the
+	 * other buttons.
+	 * 
+	 * @param event
+	 *            The event that needs to be handled
 	 */
 	@FXML
 	protected void handleButtonAction(ActionEvent event) {
 		System.out.println("Button Pressed!");
 		// When submit is pressed, calls the submit toast method
-		if (event.toString().equals(SUBMIT)){
+		if (event.toString().equals(SUBMIT)) {
 			myController.submitToast();
 		}
 	}
-	
+
 	/**
-	 * Handles button action.
-	 * Calls to the GameController to handle the button press.
+	 * Handles button action. Calls to the GameController to handle the button
+	 * press.
 	 */
 	public void handleButtons() {
 		// Associate actions with each button
-		for (int i=0; i<buttonList.size(); i++) {
+		for (int i = 0; i < buttonList.length; i++) {
 			// Associate actions with each button
 			int j = i;
-	        buttonList.get(i).setOnAction(new EventHandler<ActionEvent>() {
-	            @Override
-	            public void handle(ActionEvent event) {
-	                System.out.println("Button pressed");
-	                //toastStackPane.getChildren().add(exampleRectangle);
-	                myController.handleEvent(buttonList.get(j).getText());
-	            }
-	        });
+			buttonList[i].setOnAction(new EventHandler<ActionEvent>() {
+				@Override
+				public void handle(ActionEvent event) {
+					System.out.println("Button pressed");
+					// toastStackPane.getChildren().add(exampleRectangle);
+					myController.handleButtonClickEvent(j);
+				}
+			});
 		}
 	}
-	
+
 	/**
 	 * Called as a constructor, initializes all GUI elements of the java GUI.
-	 * @param	fxmlFileLocation	The location of the fxml file to be used as a base GUI.
-	 * @param	resources			The bundle of resources to be used in additional GUI development.
+	 * 
+	 * @param fxmlFileLocation
+	 *            The location of the fxml file to be used as a base GUI.
+	 * @param resources
+	 *            The bundle of resources to be used in additional GUI
+	 *            development.
 	 */
-	@Override 
+	@Override
 	public void initialize(URL fxmlFileLocation, ResourceBundle resources) {
-		this.setUp();	
-		for (int i=0; i< buttonList.size(); i++) {
-			buttonList.get(i).setPrefSize(BUTTONSIZE_X, BUTTONSIZE_Y);
-			buttonList.get(i).setLayoutX(BUTTON_X_LAYOUT);
-			buttonList.get(i).setLayoutY(BUTTON_Y_MULTIPLIER*i + BUTTON_X_LAYOUT);
+		this.setUp();
+		for (int i = 0; i < buttonList.length; i++) {
+			buttonList[i].setPrefSize(BUTTONSIZE_X, BUTTONSIZE_Y);
+			buttonList[i].setLayoutX(BUTTON_X_LAYOUT);
+			buttonList[i].setLayoutY(BUTTON_Y_MULTIPLIER * i + BUTTON_X_LAYOUT);
 		}
 		this.setButtons();
-        this.handleButtons();
+		this.handleButtons();
+	}
+
+	/**
+	 * Takes the image to put on the toastStackPane, resize it to appropriate
+	 * size, rotate and position it randomly on the toast
+	 * 
+	 * @param image
+	 */
+	void addImageOnToast(Image image) {
+		ImageView imageview = new ImageView(image);
+		double stackPaneHeight = toastStackPane.getHeight();
+		double stackPaneWidth = toastStackPane.getWidth();
+		Random r = new Random();
+		double imageviewX = (r.nextDouble() - 0.5) * stackPaneWidth;
+		double imageviewY = (r.nextDouble() - 0.5) * stackPaneHeight;
+		imageview.setPreserveRatio(true);
+		imageview.setFitHeight(stackPaneHeight / 10.0);
+		System.out.println(imageviewX);
+		imageview.setLayoutX(300);
+		imageview.setLayoutY(300);
+		toastStackPane.getChildren().add(imageview);
 	}
 }
