dfd1671fd6d5b83bb3f87ed5c5edc384c96f9e01
Yuping Huang <yupinghyper@gmail.com>
2016-03-04 15:09:13 -0600

    Added CurrentToast class

---
+++ src/toastGame/CurrentToast.java
@@ -0,0 +1,11 @@
+package toastGame;
+
+/**
+ * The model that keeps track of the current toast displayed on
+ * the view. Should be observed by the view.
+ * @author Yuping Huang
+ *
+ */
+class CurrentToast {
+
+}
