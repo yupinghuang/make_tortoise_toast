8f74409dcbe404cacf7e52764918511708cb3b99
Yuping Huang <yupinghyper@gmail.com>
2016-03-08 09:14:28 -0600

    ooops fix a typo in GameModel

--- src/toastGame/GameModel.java
+++ src/toastGame/GameModel.java
@@ -13,7 +13,7 @@ class GameModel {
 	private Toast toast;
 	private GameController controller;
 	private ViewInitializer view;
-	private Scorer scorer;
+	private ScoreTracker scoreTracker;
 
 	/**
 	 * instantiates the user's toast in view
